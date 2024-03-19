521c99b371fe0d19a01dbc97779f4861424f0608
AlexNg <contact@ngjx.org>
2024-03-19 16:40:30 +0800

    refactor: Use bug-report template

--- setup.sh
+++ setup.sh
@@ -115,4 +115,4 @@ esac
 
 echo
 echo "Done!
-If you encounter any issues, please report it here: https://github.com/caffeine-addictt/template/issues/new"
+If you encounter any issues, please report it here: https://github.com/caffeine-addictt/template/issues/new?assignees=caffeine-addictt&labels=Type%3A+Bug&projects=&template=1-bug-report.md&title=[Bug]+"
