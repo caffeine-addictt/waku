f88f71af1306b68b23d1783e8386d877328c09b9
AlexNg <contact@ngjx.org>
2024-08-28 00:12:41 +0800

    feat: Add root command
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ cmd/commands/root.go
@@ -0,0 +1,7 @@
+package commands
+
+import "github.com/spf13/cobra"
+
+// To initialize all the commands as subcommands of root
+func InitCommands(root *cobra.Command) {
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,31 @@
 package cmd
 
+import (
+	"log"
+
+	"github.com/caffeine-addictt/template/cmd/commands"
+	"github.com/caffeine-addictt/template/cmd/options"
+	"github.com/spf13/cobra"
+)
+
+// The Root command
+var RootCmd = &cobra.Command{
+	Use:   "template",
+	Short: "Let's make starting new projects feel like a breeze again.",
+	Long:  "This tool helps you to create a new project from templates.",
+}
+
+// Setting up configuration
+func init() {
+	RootCmd.PersistentFlags().BoolVarP(&options.Opts.Debug, "debug", "d", false, "Debug mode")
+	RootCmd.PersistentFlags().VarP(&options.Opts.Repo, "repo", "r", "Community source repository for templates")
+
+	commands.InitCommands(RootCmd)
+}
+
 // The main entry point for the command line tool
 func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
