297bcc332c6a6e25275d1d2b7b2463875ebce7b7
AlexNg <contact@ngjx.org>
2024-08-28 21:55:32 +0800

    refactor: Move option init into struct declaration
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/options/global.go
+++ cmd/options/global.go
@@ -1,9 +1,30 @@
 package options
 
 import (
+	"fmt"
+
 	"github.com/caffeine-addictt/template/cmd/utils"
 	"github.com/caffeine-addictt/template/cmd/utils/types"
 )
+
+// The global options for the CLI
+var GlobalOpts = GlobalOptions{
+	Debug:   false,
+	Verbose: false,
+	Repo:    *types.NewValueGuardNoParsing("", "<repo>"),
+	CacheDir: *types.NewValueGuard("", func(v string) (string, error) {
+		ok, err := utils.IsDir(v)
+		if err != nil {
+			return "", err
+		}
+
+		if !ok {
+			return "", fmt.Errorf("'%s' is not a valid directory", v)
+		}
+
+		return v, nil
+	}, "<path>"),
+}
 
 type GlobalOptions struct {
 	// The repository Url to use
