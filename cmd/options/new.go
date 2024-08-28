596127092d4ff8f9578213bd0699ed3d2a07e0a8
AlexNg <contact@ngjx.org>
2024-08-28 22:31:56 +0800

    feat: Add branch option
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/options/new.go
+++ cmd/options/new.go
@@ -10,6 +10,7 @@ import (
 // The options for the new command
 var NewOpts = NewOptions{
 	Repo:      *types.NewValueGuardNoParsing("", types.REPO),
+	Branch:    *types.NewValueGuardNoParsing("", types.BRANCH),
 	Directory: *types.NewValueGuardNoParsing("", types.PATH),
 	CacheDir: *types.NewValueGuard("", func(v string) (string, error) {
 		ok, err := utils.IsDir(v)
@@ -29,6 +30,9 @@ type NewOptions struct {
 	// The repository Url to use
 	// Should be this repository by default
 	Repo types.ValueGuard[string]
+
+	// The branch to use
+	Branch types.ValueGuard[string]
 
 	// The directory of the template to use
 	Directory types.ValueGuard[string]
