495690870d60f9961754f9fb5b034cde979ca3d5
Jun Xiang (カエデ) <contact@ngjx.org>
2024-08-28 19:35:18 +0800

    feat: Add caching dir and resolving option (#82)
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"os"
 
 	"github.com/caffeine-addictt/template/cmd/commands"
 	"github.com/caffeine-addictt/template/cmd/options"
@@ -13,12 +14,19 @@ var RootCmd = &cobra.Command{
 	Use:   "template",
 	Short: "Let's make starting new projects feel like a breeze again.",
 	Long:  "This tool helps you to create a new project from templates.",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if err := options.Opts.ResolveOptions(); err != nil {
+			cmd.PrintErrln(err)
+			os.Exit(1)
+		}
+	},
 }
 
 // Setting up configuration
 func init() {
-	RootCmd.PersistentFlags().BoolVarP(&options.Opts.Debug, "debug", "d", false, "Debug mode")
+	RootCmd.PersistentFlags().BoolVarP(&options.Opts.Debug, "debug", "d", false, "Debug mode [default: false]")
 	RootCmd.PersistentFlags().VarP(&options.Opts.Repo, "repo", "r", "Community source repository for templates")
+	RootCmd.PersistentFlags().VarP(&options.Opts.CacheDir, "cache", "C", "Where source repository will be cloned to [default: $XDG_CONFIG_HOME/template]")
 
 	commands.InitCommands(RootCmd)
 }
