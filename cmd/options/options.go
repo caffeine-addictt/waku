6ebcb69c41616966212044fa788035d16f9a2505
AlexNg <contact@ngjx.org>
2024-08-28 17:23:31 +0800

    feat: Add caching dir and resolving option
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/options/options.go
+++ cmd/options/options.go
@@ -1,12 +1,40 @@
 package options
 
-import "github.com/caffeine-addictt/template/cmd/utils/types"
+import (
+	"github.com/caffeine-addictt/template/cmd/utils"
+	"github.com/caffeine-addictt/template/cmd/utils/types"
+)
 
 type Options struct {
 	// The repository Url to use
 	// Should be this repository by default
 	Repo types.ValueGuard[string]
 
+	// Where the cached repositories will live
+	CacheDir types.ValueGuard[string]
+
 	// Wheter or not debug mode should be enabled
 	Debug bool
+}
+
+// To resolve the options after the user has provided them
+func (o *Options) ResolveOptions() error {
+	if err := o.resolveCacheDir(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *Options) resolveCacheDir() error {
+	if o.CacheDir.Value() != "" {
+		return nil
+	}
+
+	defaultCacheDir, err := utils.GetDefaultCacheDir()
+	if err != nil {
+		return err
+	}
+
+	return o.CacheDir.Set(defaultCacheDir)
 }
--- cmd/options/root.go
+++ cmd/options/root.go
@@ -1,6 +1,9 @@
 package options
 
 import (
+	"fmt"
+
+	"github.com/caffeine-addictt/template/cmd/utils"
 	"github.com/caffeine-addictt/template/cmd/utils/types"
 )
 
@@ -8,4 +11,16 @@ import (
 var Opts = Options{
 	Debug: false,
 	Repo:  *types.NewValueGuardNoParsing("", "<repo>"),
+	CacheDir: *types.NewValueGuard("", func(v string) (string, error) {
+		ok, err := utils.IsDir(v)
+		if err != nil {
+			return "", err
+		}
+
+		if !ok {
+			return "", fmt.Errorf("'%s' is not a valid directory", v)
+		}
+
+		return v, nil
+	}, "<path>"),
 }
