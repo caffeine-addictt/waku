2189b8e93cd08b1273ae2631b8281b889331552a
AlexNg <contact@ngjx.org>
2024-08-28 22:15:45 +0800

    refactor: Move repo and cache to new command
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/options/global.go
+++ cmd/options/global.go
@@ -1,64 +1,15 @@
 package options
 
-import (
-	"fmt"
-
-	"github.com/caffeine-addictt/template/cmd/utils"
-	"github.com/caffeine-addictt/template/cmd/utils/types"
-)
-
 // The global options for the CLI
 var GlobalOpts = GlobalOptions{
 	Debug:   false,
 	Verbose: false,
-	Repo:    *types.NewValueGuardNoParsing("", "<repo>"),
-	CacheDir: *types.NewValueGuard("", func(v string) (string, error) {
-		ok, err := utils.IsDir(v)
-		if err != nil {
-			return "", err
-		}
-
-		if !ok {
-			return "", fmt.Errorf("'%s' is not a valid directory", v)
-		}
-
-		return v, nil
-	}, "<path>"),
 }
 
 type GlobalOptions struct {
-	// The repository Url to use
-	// Should be this repository by default
-	Repo types.ValueGuard[string]
-
-	// Where the cached repositories will live
-	CacheDir types.ValueGuard[string]
-
 	// Wheter or not debug mode should be enabled
 	Debug bool
 
 	// Wheter or not verbose mode should be enabled
 	Verbose bool
-}
-
-// To resolve the options after the user has provided them
-func (o *GlobalOptions) ResolveOptions() error {
-	if err := o.resolveCacheDir(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (o *GlobalOptions) resolveCacheDir() error {
-	if o.CacheDir.Value() != "" {
-		return nil
-	}
-
-	defaultCacheDir, err := utils.GetDefaultCacheDir()
-	if err != nil {
-		return err
-	}
-
-	return o.CacheDir.Set(defaultCacheDir)
 }
---
+++ cmd/options/new.go
@@ -0,0 +1,56 @@
+package options
+
+import (
+	"fmt"
+
+	"github.com/caffeine-addictt/template/cmd/utils"
+	"github.com/caffeine-addictt/template/cmd/utils/types"
+)
+
+// The options for the new command
+var NewOpts = NewOptions{
+	Repo: *types.NewValueGuardNoParsing("", "<repo>"),
+	CacheDir: *types.NewValueGuard("", func(v string) (string, error) {
+		ok, err := utils.IsDir(v)
+		if err != nil {
+			return "", err
+		}
+
+		if !ok {
+			return "", fmt.Errorf("'%s' is not a valid directory", v)
+		}
+
+		return v, nil
+	}, "<path>"),
+}
+
+type NewOptions struct {
+	// The repository Url to use
+	// Should be this repository by default
+	Repo types.ValueGuard[string]
+
+	// Where the cached repositories will live
+	CacheDir types.ValueGuard[string]
+}
+
+// To resolve the options after the user has provided them
+func (o *NewOptions) ResolveOptions() error {
+	if err := o.resolveCacheDir(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *NewOptions) resolveCacheDir() error {
+	if o.CacheDir.Value() != "" {
+		return nil
+	}
+
+	defaultCacheDir, err := utils.GetDefaultCacheDir()
+	if err != nil {
+		return err
+	}
+
+	return o.CacheDir.Set(defaultCacheDir)
+}
