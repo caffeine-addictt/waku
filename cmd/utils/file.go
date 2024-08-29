b485629fd3d906e5c7f38ffe7d659cde065ba83c
AlexNg <contact@ngjx.org>
2024-08-29 23:49:09 +0800

    feat: Add isExecutable check
    * also abstracted out RW perm checks 0o200 & 0o400
    
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/utils/file.go
+++ cmd/utils/file.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 )
 
@@ -18,13 +19,55 @@ func IsDir(path string) (bool, error) {
 		return false, fmt.Errorf("%v is not a directory", path)
 	}
 
-	mode := fileinfo.Mode()
-	if mode&0o400 == 0 {
-		return false, fmt.Errorf("directory %v is not readable", path)
-	}
-	if mode&0o200 == 0 {
-		return false, fmt.Errorf("directory %v is not writable", path)
+	err = CheckRW(fileinfo.Mode())
+	if err != nil {
+		return false, fmt.Errorf("file %v is %v", fileinfo.Name(), err)
 	}
 
 	return true, nil
+}
+
+func IsExecutableFile(path string) (bool, error) {
+	fileinfo, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	if fileinfo.IsDir() {
+		return false, fmt.Errorf("%v is not a file", path)
+	}
+
+	err = CheckRW(fileinfo.Mode())
+	if err != nil {
+		return false, fmt.Errorf("file %v is %v", fileinfo.Name(), err)
+	}
+
+	return true, nil
+}
+
+// Check for rw------- perms
+func CheckRW(mode fs.FileMode) error {
+	if mode&0o400 == 0 {
+		return fmt.Errorf("not readable")
+	}
+	if mode&0o200 == 0 {
+		return fmt.Errorf("not writable")
+	}
+	return nil
+}
+
+// Check for rwx------ perms
+func CheckRWX(mode fs.FileMode) error {
+	if err := CheckRW(mode); err != nil {
+		return nil
+	}
+
+	if mode&0o100 == 0 {
+		return fmt.Errorf("not executable")
+	}
+
+	return nil
 }
