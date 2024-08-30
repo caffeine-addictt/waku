1e737653bfc7d63312fadc693ca95d4b58343b56
AlexNg <contact@ngjx.org>
2024-08-31 03:06:10 +0800

    feat: Improve string cleaning to strip esc chars
    * Updated tests
    * Added bechmarks
    
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/utils/string.go
+++ cmd/utils/string.go
@@ -1,16 +1,62 @@
 package utils
 
-// Escapes the given string for use in a shell command,
-// should ensure that only ever 1 command is executed
+import "strings"
+
+// Invokes CleanString with terminal-specific operators
+// like ;|&$.
 func EscapeTermString(s string) string {
-	var newS string
-	for _, v := range s {
-		switch v {
-		case '|', '\n', '\r', '&', ';', '`', '$':
-			newS += " "
-		default:
-			newS += string(v)
-		}
+	return CleanString(s, '|', '&', ';', '`', '$')
+}
+
+// Escapes the given string from common escape sequences
+// in 0(n) time, no regex.
+//
+//	\x1b[31mfoo\x1b[0m -> foo
+//	\n\r\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x7f -> empty
+func CleanString(s string, othersToEscape ...rune) string {
+	var newS strings.Builder
+
+	esc := make(map[byte]struct{}, len(othersToEscape))
+	for _, v := range othersToEscape {
+		esc[byte(v)] = struct{}{}
 	}
-	return newS
+
+	var i int
+	for i < len(s) {
+		switch s[i] {
+		case '\x1b': // handle ANSI escape sequences
+			i++
+
+			if i < len(s) && s[i] == '[' {
+				i++
+
+				seek := true
+				for i < len(s) && seek {
+					switch s[i] {
+					case 'm', 'A', 'B', 'C', 'D', 'H', 'f', 'J', 'K', 'c', 'n', 's', 'u':
+						seek = false
+					default:
+						i++
+					}
+				}
+
+				i++
+			}
+			continue
+
+		case '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', // Control characters
+			'\x08', '\x0B', '\x0C', '\x0E', '\x0F', '\n', '\r',
+			'\x7f': // Delete character
+			i++
+			continue
+		}
+
+		if _, ok := esc[s[i]]; !ok {
+			newS.WriteByte(s[i])
+		}
+
+		i++
+	}
+
+	return newS.String()
 }
--- cmd/utils/string_test.go
+++ cmd/utils/string_test.go
@@ -12,22 +12,87 @@ func TestEscapingTermString(t *testing.T) {
 		out  string
 		rule string
 	}{
-		{"git | sed", "git   sed", "escape pipes"},
-		{"a & b", "a   b", "escape chaining commands"},
-		{"a && b", "a    b", "escape AND commands"},
-		{"a || b", "a    b", "escape OR operator"},
-		{"a; b", "a  b", "escape semicolons"},
-		{"a`b", "a b", "escape backticks"},
-		{"$b", " b", "escape dollar sign"},
-		{"a\nb", "a b", "escape newlines"},
-		{"a\rb", "a b", "escape carriage returns"},
-		{"|&\n\r;`$", "       ", "escape all known special characters"},
+		{"git | sed", "git  sed", "escape pipes"},
+		{"a & b", "a  b", "escape chaining commands"},
+		{"a && b", "a  b", "escape AND commands"},
+		{"a || b", "a  b", "escape OR operator"},
+		{"a; b", "a b", "escape semicolons"},
+		{"a`b", "ab", "escape backticks"},
+		{"$b", "b", "escape dollar sign"},
+		{"a\nb", "ab", "escape newlines"},
+		{"a\rb", "ab", "escape carriage returns"},
+		{"|&\n\r;`$", "", "escape all known special characters"},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.rule, func(t *testing.T) {
 			if got := utils.EscapeTermString(tc.in); got != tc.out {
 				t.Errorf("%v. EscapeTermString() = '%v', want '%v'", tc.rule, got, tc.out)
+			}
+		})
+	}
+}
+
+func TestCleaningString(t *testing.T) {
+	tt := []struct {
+		in    string
+		out   string
+		rule  string
+		extra []rune
+	}{
+		{"\r\n", "", "clean up newlines", []rune{}},
+		{"\x00", "", "clean up null bytes", []rune{}},
+		{"\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F", "", "clean up control characters", []rune{}},
+		{"\x7f", "", "clean up DEL", []rune{}},
+		{"\x1b[0m", "", "clean up full ANSI", []rune{}},
+		{"\x1b", "", "clean up not started ANSI", []rune{}},
+		{"\x1b[", "", "clean up uncontinued ANSI", []rune{}},
+		{"\x1b[0mfoo", "foo", "clean up ANSI", []rune{}},
+		{"\x1b[31mfoo\x1b[0m", "foo", "clean up ANSI", []rune{}},
+		{"\x1b[31mfoo\nbar\x1b[0m", "foobar", "clean up ANSI", []rune{}},
+		{"\x1b[0;25;42mfoo\x1b[0m", "foo", "clean up ANSI", []rune{}},
+		{"\x1b[0;25;42mfoo\x1b[0m", "foo", "clean up ANSI with multiple codes", []rune{}},
+		{"abcde", "", "ignore extra runes passed too", []rune{'a', 'b', 'c', 'd', 'e'}},
+		{"\x1b[31mfoo\x1b[0mbar", "foobar", "clean up ANSI with extra characters", []rune{}},
+		{"\x1b[42mtext\x1b[0m\x1b[43mmore\x1b[0m", "textmore", "clean up ANSI with multiple sequences", []rune{}},
+		{"\x1b[1mbold\x1b[22m\x1b[4munderline\x1b[24m", "boldunderline", "clean up bold and underline ANSI", []rune{}},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.rule, func(t *testing.T) {
+			if got := utils.CleanString(tc.in, tc.extra...); got != tc.out {
+				t.Errorf("%v. CleanString() = '%v', want '%v'", tc.rule, got, tc.out)
+			}
+		})
+	}
+}
+
+func BenchmarkCleaningString(b *testing.B) {
+	bt := []struct {
+		in    string
+		extra []rune
+	}{
+		{"\r\n", []rune{}},
+		{"\x00", []rune{}},
+		{"\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F", []rune{}},
+		{"\x7f", []rune{}},
+		{"\x1b[0m", []rune{}},
+		{"\x1b", []rune{}},
+		{"\x1b[", []rune{}},
+		{"\x1b[0mfoo", []rune{}},
+		{"\x1b[31mfoo\x1b[0m", []rune{}},
+		{"\x1b[31mfoo\nbar\x1b[0m", []rune{}},
+		{"\x1b[0;25;42mfoo\x1b[0m", []rune{}},
+		{"abcde", []rune{'a', 'b', 'c', 'd', 'e'}},
+		{"\x1b[31mfoo\x1b[0mbar", []rune{}},
+		{"\x1b[42mtext\x1b[0m\x1b[43mmore\x1b[0m", []rune{}},
+		{"\x1b[1mbold\x1b[22m\x1b[4munderline\x1b[24m", []rune{}},
+	}
+
+	for _, bc := range bt {
+		b.Run(bc.in, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = utils.CleanString(bc.in, bc.extra...)
 			}
 		})
 	}
