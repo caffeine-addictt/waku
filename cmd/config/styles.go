c93b0852fceb9bc5a4a6da5f6dba6a6e7dbfc7e2
AlexNg <contact@ngjx.org>
2024-08-31 04:13:51 +0800

    feat: Update styles struct
    * styles will accept its own ignore, labels, skip, etc as well
    * source is required and has to be valid dir path
    
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/config/styles.go
+++ cmd/config/styles.go
@@ -6,17 +6,25 @@ import (
 	"path/filepath"
 
 	"github.com/caffeine-addictt/template/cmd/utils"
+	"github.com/caffeine-addictt/template/cmd/utils/types"
 )
 
-type TemplateStyles map[string]string
+type TemplateStyles map[types.CleanString]struct {
+	Setup  *TemplateSetup    `json:"setup,omitempty"`  // Paths to executable files for post-setup
+	Ignore *TemplateIgnore   `json:"ignore,omitempty"` // The files that should be ignored when copying
+	Labels *TemplateLabel    `json:"labels,omitempty"` // The repository labels
+	Skip   *TemplateSteps    `json:"skip,omitempty"`   // The setps to skip in using the template
+	Source types.CleanString `json:"source"`           // The source template path
+}
 
 func (t *TemplateStyles) Validate(root string) error {
-	for _, pth := range *t {
-		if !filepath.IsLocal(pth) {
-			return fmt.Errorf("path is not local: %s", pth)
+	for _, style := range *t {
+		// Source
+		if !filepath.IsLocal(style.Source.String()) {
+			return fmt.Errorf("path is not local: %s", style.Source)
 		}
 
-		resolvedPath := path.Join(root, pth)
+		resolvedPath := path.Join(root, style.Source.String())
 		if resolvedPath == "." {
 			return fmt.Errorf("cannot use . as a path")
 		}
@@ -28,6 +36,18 @@ func (t *TemplateStyles) Validate(root string) error {
 
 		if !ok {
 			return fmt.Errorf("not a directory: %s", resolvedPath)
+		}
+
+		// Others
+		if style.Setup != nil {
+			if err := style.Setup.Validate(root); err != nil {
+				return err
+			}
+		}
+		if style.Ignore != nil {
+			if err := style.Ignore.Validate(root); err != nil {
+				return err
+			}
 		}
 	}
 
