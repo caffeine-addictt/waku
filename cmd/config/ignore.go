fdf441b55ac077b91f6771f894ae86c66aa3b743
AlexNg <contact@ngjx.org>
2024-09-01 01:06:30 +0800

    fix: Validation logic error
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/config/ignore.go
+++ cmd/config/ignore.go
@@ -10,17 +10,21 @@ import (
 type TemplateIgnore []string
 
 func (t *TemplateIgnore) Validate(root string) error {
-	for _, path := range *t {
-		if !filepath.IsLocal(path) {
-			return fmt.Errorf("path is not local: %s", path)
+	for path := range *t {
+		dirPath := strings.TrimSpace(path)
+
+		// handle bang
+		dirPath = strings.TrimPrefix(dirPath, "!")
+
+		// handle glob
+		isGlob := false
+		if strings.HasSuffix(dirPath, "/*") {
+			isGlob = true
+			dirPath = strings.TrimSuffix(dirPath, "/*")
 		}
 
-		dirPath := path
-		isGrep := strings.HasSuffix(path, "/*")
-
-		// Account for path/to/files/*
-		if isGrep {
-			dirPath = path[:len(path)-2]
+		if !filepath.IsLocal(dirPath) {
+			return fmt.Errorf("path is not local: %s", path)
 		}
 
 		fileinfo, err := os.Stat(dirPath)
@@ -28,7 +32,7 @@ func (t *TemplateIgnore) Validate(root string) error {
 			return fmt.Errorf("%s: %w", path, err)
 		}
 
-		if isGrep && !fileinfo.IsDir() {
+		if isGlob && !fileinfo.IsDir() {
 			return fmt.Errorf("%s: exists but is not a directory", path)
 		}
 	}
