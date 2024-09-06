e3049060f89c2cc6578b223a1c0d27cc0a0e9737
AlexNg <contact@ngjx.org>
2024-09-06 08:40:26 +0800

    refactor: Remove state and return huh.Input
    * no longer return error after asking
    
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/template/prompting.go
+++ cmd/template/prompting.go
@@ -14,44 +14,31 @@ func ProptForLicense() error {
 	return huh.NewForm().Run()
 }
 
-// ReadProjectName prompts user to enter project name
+// PromptForProjectName prompts user to enter project name
 // or returns the name from options if it's provided.
-func PromptForProjectName(name *string, projectRootDir *string) error {
-	var state string
+func PromptForProjectName(name *string, projectRootDir *string) *huh.Input {
 	if options.NewOpts.Name.Value() != "" {
-		if err := validateProjectName(options.NewOpts.Name.Value(), name, projectRootDir, &state); err == nil {
+		if err := validateProjectName(options.NewOpts.Name.Value(), name, projectRootDir); err == nil {
 			return nil
 		}
 	}
 
-	return huh.NewForm(huh.NewGroup(
-		huh.NewInput().TitleFunc(func() string {
-			if state == "" {
-				return "Name of your project"
-			}
-
-			return fmt.Sprintf("name of your project (%s)", state)
-		}, &state).Validate(func(s string) error {
-			err := validateProjectName(s, name, projectRootDir, &state)
-			state = ""
-			return err
-		}),
-	)).WithAccessible(options.GlobalOpts.Accessible).Run()
+	return huh.NewInput().Title("Name of your project").Validate(func(s string) error {
+		return validateProjectName(s, name, projectRootDir)
+	})
 }
 
-func validateProjectName(s string, name, projectRootDir, state *string) error {
+func validateProjectName(s string, name, projectRootDir *string) error {
 	s = strings.TrimSpace(s)
 	if s == "" {
 		return fmt.Errorf("project name cannot be empty")
 	}
 
-	*state = "resolving project location..."
 	pDir, err := filepath.Rel(".", strings.ToLower(strings.ReplaceAll(s, " ", "-")))
 	if err != nil {
 		return err
 	}
 
-	*state = fmt.Sprintf("checking if directory '%s' already exists...", s)
 	if ok, err := utils.PathExists(pDir); err != nil {
 		return err
 	} else if ok {
