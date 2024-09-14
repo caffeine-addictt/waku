38e53244e8ed42fbb5466f1b796d868a30bbdc97
Jun Xiang (カエデ) <contact@ngjx.org>
2024-09-14 10:36:02 +0800

    fix: Invalid 0BSD filename and improving license tests (#132)
    * test: Improve tests for licenses
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * fix: wrong filename for 0bsd
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * fix: leading space in spdx
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    ---------
    
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/license/license_test.go
+++ cmd/license/license_test.go
@@ -1,9 +1,11 @@
 package license_test
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/caffeine-addictt/waku/cmd/license"
@@ -13,13 +15,47 @@ import (
 )
 
 func TestLicenseJson(t *testing.T) {
-	fi, err := os.OpenFile(filepath.Join("..", "..", "licenses", "license.json"), os.O_RDONLY, utils.FilePerms)
+	licenseRoot := filepath.Join("..", "..", "licenses")
+	fi, err := os.OpenFile(filepath.Join(licenseRoot, "license.json"), os.O_RDONLY, utils.FilePerms)
 	assert.NoError(t, err)
+	defer fi.Close()
 
 	buf, err := io.ReadAll(fi)
 	assert.NoError(t, err)
 
-	var l license.License
+	var l struct {
+		Licenses []license.License `json:"licenses"`
+	}
 	err = json.Unmarshal(buf, &l)
 	assert.NoError(t, err)
+
+	for i, tc := range l.Licenses {
+		t.Run(fmt.Sprintf("%d - %s", i, tc.Spdx), func(t *testing.T) {
+			assert.NotEmpty(t, tc.Filename, "filename is empty")
+			assert.NotEmpty(t, tc.Name, "name is empty")
+			assert.NotEmpty(t, tc.Spdx, "spdx is empty")
+
+			// check formatting
+			assert.Equal(t, strings.TrimSpace(tc.Filename), tc.Filename, "filename poorly formatted")
+			assert.Equal(t, strings.TrimSpace(tc.Name), tc.Name, "name poorly formatted")
+			assert.Equal(t, strings.TrimSpace(tc.Spdx), tc.Spdx, "spdx poorly formatted")
+
+			for _, v := range tc.Wants {
+				s := strings.TrimSpace(v)
+				assert.NotEmpty(t, s, "wants empty")
+				assert.Equal(t, s, v, "wants poorly formatted")
+
+				assert.False(t, strings.HasPrefix(s, "["), fmt.Sprintf("want %s should not have leading [", s))
+				assert.False(t, strings.HasSuffix(s, "]"), fmt.Sprintf("want %s should not have trailing ]", s))
+			}
+
+			fi, err := os.OpenFile(filepath.Join(licenseRoot, tc.Filename), os.O_RDONLY, utils.FilePerms)
+			assert.NoError(t, err)
+			defer fi.Close()
+
+			buf, err := io.ReadAll(fi)
+			assert.NoError(t, err)
+			assert.NotEmpty(t, buf)
+		})
+	}
 }
