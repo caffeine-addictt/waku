7cbeb9f0d906a9acbc04ec4b414676642aa5b2f3
AlexNg <contact@ngjx.org>
2024-10-02 22:06:36 +0800

    refactor: Move searching to internal/
    Signed-off-by: AlexNg <contact@ngjx.org>

--- internal/searching/binary_test.go
+++ internal/searching/binary_test.go
@@ -3,7 +3,7 @@ package searching_test
 import (
 	"testing"
 
-	"github.com/caffeine-addictt/waku/cmd/utils/searching"
+	"github.com/caffeine-addictt/waku/internal/searching"
 	"github.com/stretchr/testify/assert"
 )
 
