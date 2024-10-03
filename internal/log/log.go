211392e5ed3159001f2d9c6a1a23b6de1d702056
AlexNg <contact@ngjx.org>
2024-10-03 16:37:05 +0800

    fix: VERBOSE -> INFO and trailing space fixes
    Signed-off-by: AlexNg <contact@ngjx.org>

--- internal/log/log.go
+++ internal/log/log.go
@@ -20,8 +20,8 @@ const (
 	QUIET
 
 	debugPrefix = "\x1b[34m[DEBUG]\x1b[0m "
-	infoPrefix  = "\x1b[32m[VERBOSE]\x1b[0m "
-	warnPrefix  = "\x1b[33m[WARN]\x1b[0m "
+	infoPrefix  = "\x1b[32m[INFO]\x1b[0m  "
+	warnPrefix  = "\x1b[33m[WARN]\x1b[0m  "
 	errorPrefix = "\x1b[31m[ERROR]\x1b[0m "
 )
 
