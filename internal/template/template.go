6b375145f9f57a3e1d848111fe1ef085723555bd
AlexNg <contact@ngjx.org>
2024-10-06 01:58:59 +0800

    feat: Add supported filenames
    Ref #154
    
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ internal/template/template.go
@@ -0,0 +1,21 @@
+// Waku's template related functions
+package template
+
+
+
+// Filenames is a list of all possible template filenames
+// excluding extension
+var Filenames [12]string = [12]string{
+	"waku.yml",
+	"template.yml",
+	"waku.yaml",
+	"template.yaml",
+	"waku.json",
+	"template.json",
+	".waku.yml",
+	".template.yml",
+	".waku.yaml",
+	".template.yaml",
+	".waku.json",
+	".template.json",
+}
