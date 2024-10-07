d4870283fa84c1938e616a0a8724352ba0eb26ce
AlexNg <contact@ngjx.org>
2024-10-08 02:12:40 +0800

    feat: Update new cmd dir flag
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/commands/new.go
+++ cmd/commands/new.go
@@ -241,7 +241,7 @@ func init() {
 func AddNewCmdFlags(cmd *cobra.Command) {
 	cmd.Flags().VarP(&options.NewOpts.Repo, "repo", "r", "source repository to template from")
 	cmd.Flags().VarP(&options.NewOpts.Branch, "branch", "b", "branch to clone from")
-	cmd.Flags().VarP(&options.NewOpts.Directory, "directory", "D", "directory where 'template.json' is located")
+	cmd.Flags().VarP(&options.NewOpts.Directory, "directory", "D", "directory where config is located")
 	cmd.Flags().VarP(&options.NewOpts.Name, "name", "n", "name of the project")
 	cmd.Flags().VarP(&options.NewOpts.License, "license", "l", "license to use for the project")
 	cmd.Flags().VarP(&options.NewOpts.Style, "style", "S", "which style to use")
