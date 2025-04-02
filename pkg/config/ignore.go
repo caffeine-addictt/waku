729ac803d53bf9f0af15c442580e58762d192e57
AlexNg <contact@ngjx.org>
2025-04-03 00:30:09 +0800

    fix: Unmarshalling as map instead of slice
    Signed-off-by: AlexNg <contact@ngjx.org>

--- pkg/config/ignore.go
+++ pkg/config/ignore.go
@@ -1,12 +1,14 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/caffeine-addictt/waku/internal/types"
+	"gopkg.in/yaml.v3"
 )
 
 type TemplateIgnore types.Set[string]
@@ -40,4 +42,36 @@ func (t *TemplateIgnore) Validate(root string) error {
 	}
 
 	return nil
+}
+
+// UnmarshalJSON unmarshals a JSON array into a set
+func (t *TemplateIgnore) UnmarshalJSON(data []byte) error {
+	var items []string
+	if err := json.Unmarshal(data, &items); err != nil {
+		return err
+	}
+	*t = TemplateIgnore(types.NewSet(items...))
+	return nil
+}
+
+// MarshalJSON marshals a set into a JSON array
+func (t TemplateIgnore) MarshalJSON() ([]byte, error) {
+	s := types.Set[string](t)
+	return json.Marshal(s.ToSlice())
+}
+
+// UnmarshalYAML unmarshals a YAML string into a set
+func (t *TemplateIgnore) UnmarshalYAML(node *yaml.Node) error {
+	var items []string
+	if err := node.Decode(&items); err != nil {
+		return err
+	}
+	*t = TemplateIgnore(types.NewSet(items...))
+	return nil
+}
+
+// MarshalYAML marshals a set into a YAML string
+func (t TemplateIgnore) MarshalYAML() (interface{}, error) {
+	s := types.Set[string](t)
+	return s.ToSlice(), nil
 }
