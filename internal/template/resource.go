1256a3864804265f4bc94f506541567e9f28427b
AlexNg <contact@ngjx.org>
2025-04-03 17:30:16 +0800

    feat: Implement include field
    Signed-off-by: AlexNg <contact@ngjx.org>

--- internal/template/resource.go
+++ internal/template/resource.go
@@ -29,18 +29,56 @@ func GetStyleResources(c *config.TemplateJson, s *config.TemplateStyle, configPa
 
 		ignoreRules.Union(si)
 	}
+	if s.Include != nil && len(s.Include) != 0 {
+		si := make(types.Set[string], len(s.Include))
+
+		for _, includePath := range s.Include {
+			if includePath.Ignore == nil {
+				continue
+			}
+
+			for path := range *includePath.Ignore {
+				si.Add(filepath.Join(includePath.Source.String(), path))
+			}
+		}
+
+		ignoreRules.Union(si)
+	}
 
 	// account for !.git/ in ignore rules
 	ignoreRules = ResolveGlobs(ignoreRules, types.NewSet(".git/"))
 	log.Debugf("ignore rules: %v\n", ignoreRules)
 
+	includePaths := make(map[string]string, len(s.Include)) // includePath: dir
+	if s.Include != nil {
+		for _, includePath := range s.Include {
+			log.Infof("include path: %s\n", includePath.Source.String())
+			inPths, err := getResourcePaths(filepath.Join(configParentDir, includePath.Source.String()))
+			if err != nil {
+				return nil, err
+			}
+
+			log.Debugf("resolved include paths: %v\n", inPths)
+			for p := range inPths {
+				if includePath.Directory == nil {
+					includePaths[filepath.Join(includePath.Source.String(), p)] = ""
+				} else {
+					includePaths[filepath.Join(includePath.Source.String(), p)] = includePath.Directory.String()
+				}
+			}
+		}
+	}
 	stylePaths, err := getResourcePaths(filepath.Join(configParentDir, s.Source.String()))
 	if err != nil {
 		return nil, err
 	}
-	paths := make(types.Set[string], len(stylePaths))
+
+	paths := make(types.Set[string], len(stylePaths)+len(includePaths))
 	for p := range stylePaths {
 		paths.Add(filepath.Join(s.Source.String(), p))
+	}
+	for p := range includePaths {
+		paths.Add(p)
 	}
 	log.Debugf("unfiltered paths: %v\n", paths)
 
@@ -50,9 +88,15 @@ func GetStyleResources(c *config.TemplateJson, s *config.TemplateStyle, configPa
 	resources := make([]types.StyleResource, 0, len(filteredPaths))
 	for v := range filteredPaths {
 		parts := strings.Split(v, "/")
+		parts = parts[min(len(parts), 1):]
+
+		if dirPrepend, ok := includePaths[v]; ok {
+			parts = append([]string{dirPrepend}, parts...)
+		}
+
 		resources = append(resources, types.StyleResource{
 			TemplateResourceRelPath: v,
-			TemplatedProjectRelPath: strings.Join(parts[min(len(parts), 1):], "/"),
+			TemplatedProjectRelPath: strings.Join(parts, "/"),
 		})
 	}
 
