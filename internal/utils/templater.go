c2d800301ca026dd6c2d9a456557c810619c6335
AlexNg <contact@ngjx.org>
2026-02-13 02:01:13 +0800

    feat: Add some default functions
    Signed-off-by: AlexNg <contact@ngjx.org>

--- internal/utils/templater.go
+++ internal/utils/templater.go
@@ -3,9 +3,69 @@ package utils
 import (
 	"bufio"
 	"context"
+	"fmt"
+	"strconv"
 	"strings"
 	"text/template"
+	"time"
+
+	"github.com/goccy/go-json"
 )
+
+func take2NumArgs(f func(float64, float64) float64) func(string, string) string {
+	return func(a, b string) string {
+		aF, _ := strconv.ParseFloat(a, 64)
+		bF, _ := strconv.ParseFloat(b, 64)
+
+		return fmt.Sprintf("%v", f(aF, bF))
+	}
+}
+
+var funcMap = template.FuncMap{
+	// String
+	"toLower":   strings.ToLower,
+	"tpUpper":   strings.ToUpper,
+	"toTitle":   strings.ToTitle,
+	"trim":      strings.TrimSpace,
+	"replace":   strings.ReplaceAll,
+	"contains":  strings.Contains,
+	"hasPrefix": strings.HasPrefix,
+	"hasSuffix": strings.HasSuffix,
+	"join":      strings.Join,
+	"split":     strings.Split,
+	"slug": func(s string) string {
+		s = strings.ToLower(s)
+		return strings.ReplaceAll(s, " ", "-")
+	},
+
+	// Arrhythmic
+	"add": take2NumArgs(func(a, b float64) float64 { return a + b }),
+	"sub": take2NumArgs(func(a, b float64) float64 { return a - b }),
+	"mul": take2NumArgs(func(a, b float64) float64 { return a * b }),
+	"div": take2NumArgs(func(a, b float64) float64 { return a / b }), // no panic IEEE-754
+	"ternary": func(cond bool, a, b any) any {
+		if cond {
+			return a
+		}
+		return b
+	},
+
+	"timefmt": func(t time.Time, layout string) string {
+		return t.Format(layout)
+	},
+
+	"json": func(v any) string {
+		b, _ := json.MarshalIndent(v, "", "  ")
+		return string(b)
+	},
+
+	"default": func(a, b string) string {
+		if a == "" {
+			return b
+		}
+		return a
+	},
+}
 
 // This handles consuming a file stream,
 // templating it and writing it to writer.
@@ -28,7 +88,7 @@ func ParseTemplateFile(ctx context.Context, tmpl map[string]any, reader *bufio.S
 		s.WriteRune('\n')
 	}
 
-	t, err := template.New("file").Option("missingkey=error").Delims("{{{", "}}}").Parse(s.String())
+	t, err := template.New("file").Option("missingkey=error").Delims("{{{", "}}}").Funcs(funcMap).Parse(s.String())
 	if err != nil {
 		return err
 	}
